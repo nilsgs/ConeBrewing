c5d3b873067345628bd558ddf2c45bc9cfc30d6d
Nils Georg Skutle <nilsgs@gmail.com>
2012-08-28 00:46:43 +0200

    added basic layout

--- ConeBrewing.MVC/App_Start/RouteConfig.cs
+++ ConeBrewing.MVC/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace ConeBrewing.MVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Beers",
+                url: "Beer/{id}",
+                defaults: new { controller = "Beer", action = "Details" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ ConeBrewing.MVC/Controllers/BeerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConeBrewing.MVC.Controllers
+{
+    public class BeerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
