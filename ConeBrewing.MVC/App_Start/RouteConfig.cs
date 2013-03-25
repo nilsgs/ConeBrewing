999242ceff7191dbe04861ff19434b95ccea87a0
Nils Georg Skutle <nilsgs@gmail.com>
2013-03-25 19:29:40 +0100

    Added ABV calculator

--- ConeBrewing.MVC/App_Start/RouteConfig.cs
+++ ConeBrewing.MVC/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace ConeBrewing.MVC
@@ -22,7 +18,7 @@ namespace ConeBrewing.MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Calculator", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ConeBrewing.MVC/Controllers/CalculatorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace ConeBrewing.MVC.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Abv()
+        {
+            return PartialView("_AbvCalc");
+        }
+
+        public ActionResult Ibu()
+        {
+            return PartialView("_IbuCalc");
+        }
+    }
+}
